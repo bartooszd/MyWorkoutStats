4c568968ad46dbb7be528b8a52695685b0135ef8
bartooszd <chory345Mis>
2018-07-14 22:57:31 +0200

    Working version

---
+++ app/src/main/java/com/example/bart/MyWorkoutHistory/CreditSettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.bart.MyWorkoutHistory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bart.firstapp2.R;
+
+public class CreditSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_settings);
+    }
+}
